f751310d338ed39d04bc2fbd0df1c5e699a16b69
ismail hossain <ismailhsan45@gmail.com>
2021-06-09 12:12:05 +0600

    done

--- server/router/router.js
+++ server/router/router.js
@@ -5,32 +5,32 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { verifyuser } = require('../../config/protectroute');
 let loguserId;
-const fs = require('fs')
+const fs = require('fs');
 const Memorie = require('../models/Memorie');
 const path = require('path');
 const multer = require('multer');
-const uploadPath = path.join('public', Memorie.coverImageBasePath)
+const uploadPath = path.join('public', Memorie.coverImageBasePath);
 const upload = multer({
     dest: uploadPath,
-})
+});
 router.get('/', verifyuser, (req, res) => {
     const uid = req.userId;
     User.findOne({ _id: uid }).then(user => {
         res.render('dashboard', { user });
-    })
-})
+    });
+});
 
 router.get('/login', (req, res) => {
     res.render('login')
-})
+});
 router.get('/logout', (req, res) => {
     res.cookie('user', "", { maxAge: 10 })
     res.redirect('/login');
-})
+});
 
 router.get('/register', (req, res) => {
     res.render('register')
-})
+});
 router.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
     const error = [];
@@ -62,7 +62,7 @@ router.post('/register', async (req, res) => {
 const maxage = 60 * 60 * 24 * 3;
 const createToken = (id) => {
     return jwt.sign({ id }, process.env.SECREET_TOKEN, { expiresIn: maxage });
-}
+};
 
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
@@ -84,21 +84,21 @@ router.post('/login', (req, res) => {
             res.render('login', { error });
         }
     })
-})
+});
 
 router.get('/memo/:id', async (req, res) => {
     const uid = req.params.id;
     const data = await Memorie.find({ uid }).sort({ date: 'desc' })
     res.render('memo', { data });
-})
+});
 
 router.get('/add', verifyuser, (req, res) => {
     const uid = req.userId;
     User.findOne({ _id: uid }).then(user => {
         loguserId = user._id
         res.render('add', { loguserId: user._id })
-    })
-})
+    });
+});
 
 router.post('/add', upload.single('image'), async (req, res) => {
     const filename = req.file != null ? req.file.filename : null
@@ -107,7 +107,7 @@ router.post('/add', upload.single('image'), async (req, res) => {
         desc: req.body.desc,
         uid: req.body.uid,
         imagename: filename
-    })
+    });
     try {
         const newMemo = await memo.save();
         res.redirect(`/memo/${loguserId}`);
@@ -118,14 +118,14 @@ router.post('/add', upload.single('image'), async (req, res) => {
         }
         res.render('add', { loguserId })
     }
-})
+});
 
 function removeBookCover(filename) {
     fs.unlink(path.join(uploadPath, filename), err => {
         if (err) {
             console.log(err);
         }
-    })
-}
+    });
+};
 
 module.exports = router;
